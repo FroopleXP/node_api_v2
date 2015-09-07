97663fdc13508f4b5031194ff8476e7c9e2a5fe8
FroopleXP <connor@frooplexp.com>
2015-09-07 09:41:42 +0100

    Updated .gitignore and added student statius

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var db = require('./mysql_conn.js');
 
 exports.get_all = function(req, res) {
-	db.query("SELECT fname, lname, id FROM users LIMIT 10", function(err, itm) {
+	db.query("SELECT fname, lname, bar_id, State FROM users LIMIT 20", function(err, itm) {
 		if (err) throw err;
 		res.send(itm);
 	});
@@ -9,7 +9,7 @@ exports.get_all = function(req, res) {
 
 exports.get_by_fname = function(req, res) {
 	var name = req.params.fname;
-	db.query("SELECT lname, id FROM users WHERE fname = ? LIMIT 10", [name], function(err, itm) {
+	db.query("SELECT lname, bar_id, State FROM users WHERE fname = ? LIMIT 10", [name], function(err, itm) {
 		if (err) throw err;
 		res.send(itm);
 	});
@@ -17,7 +17,7 @@ exports.get_by_fname = function(req, res) {
 
 exports.get_by_lname = function(req, res) {
 	var name = req.params.lname;
-	db.query("SELECT fname, id FROM users WHERE lname = ? LIMIT 10", [name], function(err, itm) {
+	db.query("SELECT fname, bar_id, State FROM users WHERE lname = ? LIMIT 10", [name], function(err, itm) {
 		if (err) throw err;
 		res.send(itm);
 	});
